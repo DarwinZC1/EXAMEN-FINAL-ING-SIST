11ac870c279d3d76f08642e9624bdd23b6e4cb51
DarwinZC1 <132626111+DarwinZC1@users.noreply.github.com>
2023-05-31 22:04:12 -0400

    Add files via upload

---
+++ archivo.js
@@ -0,0 +1,45 @@
+//INICIO
+
+ 
+
+
+
+ let correo = prompt("Ingrese un correo:")
+ if (+correo [0] >= 0 && +correo [0] <= 9) {
+    } else {
+    let correo2 = correo.toLowerCase()  
+    if (correo2.endsWith("cumbre.edu.bo")) {
+     let password = prompt("Ingrese contraseña")
+    if (password.length > 8) {
+    if (password.startsWith("$*")) {
+    if (SolicitudPost == true) {
+    let post = prompt("ingrese su primer post")
+    if (post.length >0 ) {
+    let cadenaPost = "Mi primer post en JS"
+    alert("Mi primer post en JS")
+    console.warn(cadenaPost.split(" "))
+    
+    }else{
+    alert("vuelva a intentarlo en (NumeroRandom) seg. \n <fechaActual>")
+    }
+
+    } else {  
+    alert("Mi primer post en JS")
+    }
+
+   }else{
+    alert("vuelva a intentarlo")
+   }
+
+
+   }else{
+    alert("vuelva a intentarlo")
+   }
+
+  }
+    }
+
+   
+    
+    
+
